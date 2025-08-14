46a5857774be9a2856c9f30cf5a7b3f094b7575e
long12345mk <125982470+long12345mk@users.noreply.github.com>
2025-08-14 17:05:00 +0700

    Update Places.jsx

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -18,7 +18,7 @@ export default function Places({
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
                 <img
-                  src={`http://localhost:3000/${place.image.src}`}
+                  src={`http://localhost:5000/images/${place.image.src}`}
                   alt={place.image.alt}
                 />
                 <h3>{place.title}</h3>
